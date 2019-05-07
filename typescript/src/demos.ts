a8d5eff8eb656218104452b886a19ccad422d7ca
mark <sund@haidilao.com>
2019-05-07 10:59:27 +0800

    test

--- typescript/src/demos.ts
+++ typescript/src/demos.ts
@@ -1,12 +1,12 @@
 // string类型
-let stringType: string = "seven"
+const stringType: string = "seven"
 // any类型
-let anyType: any = "hello"
+const anyType: any = "hello"
 // 定义整型变量
-let numberType: number = 18
-let a: number = 178.5
+const numberType: number = 18
+const a: number = 178.5
 // 定义array型并指定元素都未number变量
-let list: Array<number> = [1, 2, 3]
+const list: Array<number> = [1, 2, 3]
 
 console.log(typeof list[1])
 // 定义枚举类型
@@ -16,25 +16,29 @@ enum REN {
 	yao = "d"
 }
 //
-let anyThing: any = "hello"
+const anyThing: any = "hello"
+const ab: string = "ddddd"
 // 定义多个类型
 let num_str: number | string | Array<number>
 console.log((num_str = [11, 22, 33]))
 
 const arr: number[] = [1, 2, 3]
-let arr1: Array<number> = [1, 1, 2222]
+const arr1: Array<number> = [1, 1, 2222]
 // 定义元组
 let arr2: [string, number, boolean]
 arr2 = ["1", 2, true] // typescript 定义值时会做类型检测
 // 定义函数，指定函数参数类型
-let test = function(params: string) {
-	return params + "fucking"
+const test = function(params: string) {
+	return `${params}fucking`
 }
 // 定义void类型
-let test1: null = null
+const test1: null = null
 
-let booleanType: boolean = false
-
+const booleanType: boolean = false
+let num: number = 1
+if (num == 2) {
+	console.log(num)
+}
 console.log(stringType)
 console.log(numberType)
 console.log(anyType)
@@ -45,7 +49,7 @@ console.log(test1)
 console.log(test("ssssss"))
 console.log(booleanType)
 
-let mui = {
+const mui = {
 	say() {
 		console.log("测试。。。")
 	},
@@ -71,24 +75,23 @@ mui.sayStr("qqqqqqqqqqqqq")
 // console.log(calc.next())
 // console.log(calc.next(10))
 
+// // proxy也就是代理操作
+// const validator = {
+// 	set(obj: any, prop: any, value: any) {
+// 		if (prop === "age") {
+// 			if (!Number.isInteger(value)) {
+// 				throw new TypeError("The age is not an integer")
+// 			}
+// 			if (value > 200) {
+// 				throw new RangeError("The age seems invalid")
+// 			}
+// 		}
+// 		console.log(obj, prop, value)
+// 		obj[prop] = obj[prop] + 100
+// 		return true
+// 	}
+// }
 
-// proxy也就是代理操作
-let validator = {
-	set: function(obj: any, prop: any, value: any) {
-		if (prop === "age") {
-			if (!Number.isInteger(value)) {
-				throw new TypeError("The age is not an integer")
-			}
-			if (value > 200) {
-				throw new RangeError("The age seems invalid")
-			}
-		}
-		console.log(obj, prop, value)
-		obj[prop] = obj[prop] + 100
-		return true
-	}
-}
-
-let person = new Proxy({ age: 1 }, validator)
-person.age = 12
-console.log(person.age)
+// const person = new Proxy({ age: 1 }, validator)
+// person.age = 12
+// console.log(person.age)
